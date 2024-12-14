42e7c4c82ed14d4b613e043b870303dc4647a19d
Aditya Shibu <as2397@hw.ac.uk>
2024-12-14 10:27:53 +0400

    Added files for data import

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spaceDebrisDetection
-Creating a model for detection of space debris 
+
+Creating a model for detection of space debris
+
+# How to run
+
+- Run the dataImport.py to import the dataset from kaggle
---
+++ dataImport.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Replace 'sadianawar/debris-detection-dataset' with the correct dataset identifier
+debris_dataset = kagglehub.dataset_download('sadianawar/debris-detection-dataset')
+
+print('Dataset downloaded at:', debris_dataset)
