50aa452a9eefbd8a04abb732361aa775285e2198
Aditya Shibu <as2397@hw.ac.uk>
2024-12-15 14:37:04 +0400

    Completed model

--- dataImport.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Replace 'sadianawar/debris-detection-dataset' with the correct dataset identifier
-debris_dataset = kagglehub.dataset_download('sadianawar/debris-detection-dataset')
-
-print('Dataset downloaded at:', debris_dataset)
